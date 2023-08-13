3558be343290e7c950ccd7cd106c6fef10fddad6
ElshanRustamzada <elshanrustamzada22@gmail.com>
2023-08-13 13:57:40 +0400

    add Slider Create button

--- EduHome/EduHome/Areas/Admin/Controllers/SlidersController.cs
+++ EduHome/EduHome/Areas/Admin/Controllers/SlidersController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace EduHome.Areas.Admin.Controllers
 {
@@ -19,6 +20,27 @@ namespace EduHome.Areas.Admin.Controllers
         {
             List<Slider> sliders = await _db.Sliders.ToListAsync();
             return View(sliders);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Slider slider)
+        {
+            #region isExist
+            bool isExist = await _db.Sliders.AnyAsync(x => x.Name == slider.Name);
+            if (isExist)
+            {
+                ModelState.AddModelError("Name", "This slider is already exist");
+                return View();
+            } 
+            #endregion
+
+            await _db.Sliders.AddAsync(slider);
+            await _db.SaveChangesAsync();
+            return RedirectToAction("Index");
         }
     }
 }
--- EduHome/EduHome/Models/Slider.cs
+++ EduHome/EduHome/Models/Slider.cs
@@ -1,8 +1,12 @@
-﻿namespace EduHome.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace EduHome.Models
 {
     public class Slider
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "don't null")]
         public string Name { get; set; }
         public string Description { get; set; }
         public string Image { get; set; }
